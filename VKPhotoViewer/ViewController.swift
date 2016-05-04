cf74f3e722985f5ee281bcb5f195fef3a9239e07
Evgeny Starikov <starikovea@yandex.ua>
2016-05-04 21:38:25 +0300

    Initial Commit

---
+++ VKPhotoViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VKPhotoViewer
+//
+//  Created by Евгений Стариков on 04.05.16.
+//  Copyright © 2016 Starikov Evgen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
